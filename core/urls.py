aff8b77b95b071538344cb975089d37045adc62f
Lyumm42 <sindzi810@gmail.com>
2025-04-13 16:09:47 +0600

    2 commit

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for library project.
+URL configuration for apps project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
